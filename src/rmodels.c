9f5296261b45f288cc5b4826e2cfd91c4a759f0c
Jeffery Myers <jeffm2501@gmail.com>
2024-08-21 17:47:49 -0700

    invert V coordinate

--- src/rmodels.c
+++ src/rmodels.c
@@ -4244,8 +4244,10 @@ static Model LoadOBJ(const char* fileName)
 			for (int i = 0; i < 2; i++)
 				model.meshes[meshIndex].texcoords[localMeshVertexCount * 2 + i] = objAttributes.texcoords[texcordIndex * 2 + i];
 
-			for (int i = 0; i < 4; i++)
-				model.meshes[meshIndex].colors[localMeshVertexCount * 4 + i] = 255;
+            model.meshes[meshIndex].texcoords[localMeshVertexCount * 2 + 1] = 1.0f - model.meshes[meshIndex].texcoords[localMeshVertexCount * 2 + 1];
+
+            for (int i = 0; i < 4; i++)
+                model.meshes[meshIndex].colors[localMeshVertexCount * 4 + i] = 255;
 
 			faceVertIndex++;
 			localMeshVertexCount++;
